b30e24fe5400fc0a94ba7577ebccde1b7e5edf70
Anastasia <nastasia199126@gmail.com>
2021-08-14 00:20:21 +0300

    m

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,24 +1,24 @@
 import PropTypes, { shape } from 'prop-types';
-// import s from './FriendList.module.css'
+import s from './TransactionHistory.module.css'
 
 
 const TransactionHistory = ({items}) => {
     return (
-        <table className="transaction-history">
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
+        <table className={s.transactionHistory}>
+  <thead className={s.table}>
+    <tr className={s.tr}>
+      <th className={s.th}>Type</th>
+      <th className={s.th}>Amount</th>
+      <th className={s.th}>Currency</th>
     </tr>
   </thead>
 
         <tbody>
           {items.map(item => (
-    <tr key={item.id}> 
-      <td>{item.type}</td>
-      <td>{item.amount}</td>
-      <td>{item.currency}</td>
+    <tr className={s.tr} key={item.id}> 
+      <td className={s.td}>{item.type}</td>
+      <td className={s.td}>{item.amount}</td>
+      <td className={s.td}>{item.currency}</td>
     </tr>
           ))}
     
